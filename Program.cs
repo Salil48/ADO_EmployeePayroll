0f6dbe6729c2840b04bb4588003070b955a34bb7
“Salil48” <salilchharia@gmail.com>
2023-02-04 16:49:22 +0530

    Retrieve Data

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ADO_Employee_Payroll.ADO_Employee_Payroll;
 
 namespace ADO_Employee_Payroll
 {
@@ -6,7 +7,10 @@ namespace ADO_Employee_Payroll
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Payroll Services using ADO.NET");
+            Console.WriteLine("Welcome to Payroll Services using ADO!");
+            //Create oobject for Employee Repository
+            EmployeeRepository employeeRepository = new EmployeeRepository();
+            employeeRepository.GetSqlData();
 
         }
     }
